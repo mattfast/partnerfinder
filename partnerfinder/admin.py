ab5b3ad5eb3a5121cd108bf3a95ee35d870bd979
Matthew Fastow <matthewfastow@gmail.com>
2020-02-24 21:23:38 -0500

    added admin db permissions

--- partnerfinder/admin.py
+++ partnerfinder/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(TimeSlot)
+admin.site.register(Availability)
+admin.site.register(Enrollment)
+admin.site.register(Instruction)
+admin.site.register(PreviousCourse)
+admin.site.register(Assignment)
+admin.site.register(Posting)
