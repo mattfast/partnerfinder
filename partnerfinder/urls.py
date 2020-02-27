3037bae4955ace1adffd6c37b36fe2e11fff2b50
Matthew Fastow <matthewfastow@gmail.com>
2020-02-27 15:37:02 -0500

    added templates, urls, view methods

--- partnerfinder/urls.py
+++ partnerfinder/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('classes/', views.classes, name='classes'),
+    path('classes/<int:class_id>/', views.classinfo, name='classinfo'),
+    path('assignment/<int:assignment_id>', views.assignmentinfo, name='assignmentinfo'),
+    path('posting/<int:posting_id>', views.postinginfo, name='postinginfo'),
+
+    path('createclass/', views.createclass, name='createclass'),
+    path('classes/<int:class_id>/createassignment/', views.createassignment, name='createassignment'),
+    path('assignment/<int:assignment_id>/createposting/', views.createposting, name='createposting'),
+
 ]
--- partnerfinder/views.py
+++ partnerfinder/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import Http404
+
+from .models import *
 
 
 def index(request):
-    return HttpResponse("Hello! Welcome to PartnerFinder.")
+    return render(request, 'index.html')
+
+def classes(request):
+	return render(request, 'classes.html')
+
+def classinfo(request, class_id):
+	return render(request, 'classinfo.html')
+
+def assignmentinfo(request, assignment_id):
+	return render(request, 'assignmentinfo.html')
+
+def createclass(request):
+	return render(request, 'classinfo.html')
+
+def createassignment(request, class_id):
+	return render(request, 'assignmentinfo.html')
+
+def createposting(request, assignment_id):
+	return render(request, 'postinginfo.html')
